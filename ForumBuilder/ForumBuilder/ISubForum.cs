1e679b4965424b665b59d49b8d1228cc226c43f2
gal tfilin <galtfilin@gmail.com>
2016-03-31 11:31:38 +0300

    initial interfaces.. we still got work to do about that

---
+++ ForumBuilder/ForumBuilder/ISubForum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controllers;
+
+namespace ForumBuilder
+{
+    interface ISubForum
+    {
+        Boolean dismissModerator(IModerderator dismissedModerator);
+        Boolean createThread(String headLine, String Content);
+    }
+}
