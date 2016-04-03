0a8bfbcbd4f877a2c38bd19217cc14423af6e802
avivasi <aviv22a@gmail.com>
2016-04-03 12:05:11 +0300

    service

---
+++ ForumBuilder/ForumBuilder/Users/IUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForumBuilder.Forum;
+
+namespace ForumBuilder.Users
+{
+    public interface IUser
+    {
+        String getUserName();
+        Boolean addFriend(IUser newFriend);
+        Boolean deleteFriend(IUser deletedFriend);
+        Boolean notifyUserViaMail();
+        Boolean sendPrivateMessage(String userName, String content);
+        Boolean banMember(String userName, String forumName);
+        Boolean createSubForum(String subForumName, IForum forum, List<String> moderators);
+        Boolean changePolicy(String newPolicy, IForum forum);
+        Boolean isAdmin();
+        Boolean isMember();
+        Boolean createThread();
+        Boolean dismissModerator(IUser dismissedModerator);
+        Boolean deleteThread(IThread toDelete);
+
+    }
+}
---
+++ ForumBuilder/ForumBuilder/Users/SuperUser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForumBuilder.Forum;
+
+namespace ForumBuilder.Users
+{
+    public class SuperUser : ISuperUser
+    {
+        public SuperUser()
+        {
+
+        }
+
+        public Boolean createForum(String forumName, String descrption, String forumPolicy, String forumRules, List<String> administrators)
+        {
+            return true;
+        }
+
+        public String getUserName()
+        {
+            return "";
+        }
+        public Boolean addFriend(IUser newFriend)
+        {
+            return true;
+        }
+        public Boolean deleteFriend(IUser deletedFriend)
+        {
+            return true;
+        }
+        public Boolean notifyUserViaMail()
+        {
+            return true;
+        }
+        public Boolean sendPrivateMessage(String userName, String content)
+        {
+            return true;
+        }
+        public Boolean banMember(String userName, String forumName)
+        {
+            return true;
+        }
+        public Boolean createSubForum(String subForumName, IForum forum, List<String> moderators)
+        {
+            return true;
+        }
+        public Boolean changePolicy(String newPolicy, IForum forum)
+        {
+            return true;
+        }
+        public Boolean isAdmin()
+        {
+            return true;
+        }
+        public Boolean isMember()
+        {
+            return true;
+        }
+        public Boolean createThread()
+        {
+            return true;
+        }
+        public Boolean dismissModerator(IUser dismissedModerator)
+        {
+            return true;
+        }
+        public Boolean deleteThread(IThread toDelete)
+        {
+            return true;
+        }
+
+    }
+}
