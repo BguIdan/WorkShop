4ce2383502f8faecc3fae5bcfe28d049f0b5700a
aviassaf <guy30aviassaf@gmail.com>
2016-04-10 10:31:23 +0300

    fix prob and impl

---
+++ ForumBuilder/ForumBuilder/BL_Back_End/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForumBuilder.BL_Back_End
+{
+    public class User
+    {
+        private String _userName;
+        private String _password;
+        private String _email;
+        private List<String> _friends; 
+
+        public User(String userName, String password, String email)
+        {
+            _userName = userName;
+            _password = password;
+            _email = email;
+            _friends = new List<String>();
+    }
+
+        public String userName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        public String password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public String email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public List<String> friends
+        {
+            get { return _friends; }
+            set { _friends = value; }
+        }
+
+    }
+}
