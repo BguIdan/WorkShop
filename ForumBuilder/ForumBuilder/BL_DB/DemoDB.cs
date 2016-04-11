36d8a48aadb327c6b2df3af95866c768c9ee72fb
aviassaf <guy30aviassaf@gmail.com>
2016-04-11 11:16:30 +0300

    add inhertiance to super user

--- ForumBuilder/ForumBuilder/BL_DB/DemoDB.cs
+++ ForumBuilder/ForumBuilder/BL_DB/DemoDB.cs
@@ -122,7 +122,7 @@ namespace ForumBuilder.BL_DB
         {
             foreach (SuperUser superUser in superUsers)
             {
-                if (superUser._userName.Equals(userName))
+                if (superUser.userName.Equals(userName))
                     return superUser;
             }
             return null;
