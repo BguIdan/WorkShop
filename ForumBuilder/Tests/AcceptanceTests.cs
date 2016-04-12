7c2d991064a2bfa9453ef2dc6aa0a7c75b44484a
gal tfilin <galtfilin@gmail.com>
2016-04-12 19:06:48 +0300

    creating Acceptance Tests class

---
+++ ForumBuilder/Tests/AcceptanceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class AcceptanceTests
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
