4765b7dcbdba41d62645adaa4dafe7fbf12afd5c
tomer1995 <tomer1995@walla.com>
2016-05-05 15:43:12 +0300

    finished uc 6,11

--- ForumBuilder/PL/SubForumWindow.xaml.cs
+++ ForumBuilder/PL/SubForumWindow.xaml.cs
@@ -1,31 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using ForumBuilder.Common.DataContracts;
+using PL.proxies;
 
 namespace PL
 {
     /// <summary>
     /// Interaction logic for SubForumWindow.xaml
     /// </summary>
-    public partial class SubForumWindow : Window
+    /// 
+    public class dataContainer
     {
-        public SubForumWindow()
+        private int Id;
+        private string Title;
+        private string Writer;
+        private string Time;
+
+        public string _title
         {
-            InitializeComponent();
+            get
+            {
+                return Title;
+            }
+
+            set
+            {
+                Title = value;
+            }
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public string _writer
         {
+            get
+            {
+                return Writer;
+            }
+
+            set
+            {
+                Writer = value;
+            }
+        }
+
+        public string _time
+        {
+            get
+            {
+                return Time;
+            }
+
+            set
+            {
+                Time = value;
+            }
+        }
+
+        public int _id
+        {
+            get
+            {
+                return Id;
+            }
+
+            set
+            {
+                Id = value;
+            }
+        }
+    }
+
+    public partial class SubForumWindow : Window
+    {
+        private PostManagerClient _pm;
+        private string _userName;
+        private int _patentId;//used for adding post;
+
+        public SubForumWindow(string fName, string sfName, string userName)//forum subforum names and userName
+        {
+            InitializeComponent();
+            _pm = new PostManagerClient();
+            forumName.Content = fName;
+            sForumName.Content = sfName;
+            _userName = userName;
+            _patentId = -1;
+        }
+
+        private void ThreadView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var grid = sender as DataGrid;
+            var selected = grid.SelectedItem as dataContainer;
+            List<PostData> posts = _pm.getAllPosts(forumName.Content.ToString(), sForumName.Content.ToString());
+            var commentTable = new List<dataContainer>();
+            foreach (PostData pd1 in posts)
+            {
+                if (pd1.id == selected._id)//needs to show the thread of this post
+                {
+                    List<int> commentsIds = pd1.commentsIds;
+                    //going over all comments to make a new table
+                    _patentId = pd1.id;
+                    foreach (int singleCommentId in commentsIds)
+                    {
+                        foreach (PostData pd2 in posts)
+                        {
+                            if (pd2.id == singleCommentId)
+                            {
+                                dataContainer dt = new dataContainer();
+                                dt._id = pd2.id;
+                                dt._title = pd2.title;
+                                dt._writer = pd2.writerUserName;
+                                dt._time = pd2.timePublished.ToString();
+                                commentTable.Add(dt);
+                            }
+                        }
+                    }
+                }
+            }
+            singleThread.ItemsSource = commentTable;
+            threadView.Visibility = Visibility.Collapsed;
+            threadTextBox.Visibility = Visibility.Collapsed;
+            addThreadButton.Visibility = Visibility.Collapsed;
+            addPostButton.Visibility = Visibility.Visible;
+            singleThread.Visibility = Visibility.Visible;
 
         }
 
@@ -46,7 +142,91 @@ namespace PL
 
         private void DataGrid_Loaded(object sender, RoutedEventArgs e)
         {
+            List<PostData> posts = _pm.getAllPosts(forumName.Content.ToString(), sForumName.Content.ToString());
+            var table = new List<dataContainer>();
+            foreach (PostData pd in posts)
+            {
+                if (pd.parentId == -1)//if its the first message in thread
+                {
+                    dataContainer dt = new dataContainer();
+                    dt._id = pd.id;
+                    dt._title = pd.title;
+                    dt._writer = pd.writerUserName;
+                    dt._time = pd.timePublished.ToString();
+                    table.Add(dt);
+                }
+            }
+            var grid = sender as DataGrid;
+            grid.ItemsSource = table;
+            addPostButton.Visibility = Visibility.Collapsed;
+            singleThread.Visibility = Visibility.Collapsed;
+            threadView.Visibility = Visibility.Visible;
+        }
 
+        private void singleThread_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            if (singleThread.Visibility == Visibility.Visible)
+            {
+                singleThread.Visibility = Visibility.Collapsed;
+                addPostButton.Visibility = Visibility.Collapsed;
+                addThreadButton.Visibility = Visibility.Visible;
+                threadView.Visibility = Visibility.Visible;
+                threadTextBox.Visibility = Visibility.Visible;
+                _patentId = -1;
+            }
+            else//needs to go back to previous page
+            {
+
+            }
+        }
+
+        private void openMessage_Click(object sender, RoutedEventArgs e)
+        {
+            var grid = singleThread;
+            var selected = grid.SelectedItem as dataContainer;
+            List<PostData> posts = _pm.getAllPosts(forumName.Content.ToString(), sForumName.Content.ToString());
+            foreach (PostData pd in posts)
+            {
+                if (pd.id == selected._id)
+                {
+                    MessageBox.Show(pd.content);
+                }
+            }
+        }
+
+        private void deleteMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var grid = singleThread;
+            var selected = grid.SelectedItem as dataContainer;
+            List<PostData> posts = _pm.getAllPosts(forumName.Content.ToString(), sForumName.Content.ToString());
+            PostData postToDelete = null;
+            foreach (PostData pd in posts)
+            {
+                if (pd.id == selected._id)
+                {
+                    postToDelete = pd;
+                }
+            }
+            if (postToDelete != null)
+            {
+                _pm.deletePost(postToDelete.id, _userName);
+            }
+            else
+            {
+                MessageBox.Show("error: couldn't find message");
+            }
+        }
+
+        private void addPostButton_Click(object sender, RoutedEventArgs e)
+        {
+            addPostAndThreadWindow win = new addPostAndThreadWindow(this, _patentId, _userName, forumName.Content.ToString(), sForumName.Content.ToString());
+            this.Visibility = Visibility.Collapsed;
+            win.Show();
         }
     }
 }
