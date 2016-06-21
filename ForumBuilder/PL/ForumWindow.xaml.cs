c74297554a7637450f7a85bbcc1d1b2c078bb49c
tomer1995 <tomer1995@walla.com>
2016-06-21 16:41:30 +0300

    moderatorFix

--- ForumBuilder/PL/ForumWindow.xaml.cs
+++ ForumBuilder/PL/ForumWindow.xaml.cs
@@ -143,10 +143,38 @@ namespace PL
             List<String> moderaorsReports = _fMC.getAdminReport(_userName, _myforum.forumName);
             numOfPosts.Text = "Number of Posts :  " + (_fMC.getAdminReportNumOfPOst(_userName, _myforum.forumName)).ToString();
             //reportListBox.Items.Clear();
+            double height = 0;
+       
             foreach (string report in moderaorsReports)
             {
-                reportListBox.Items.Add(report);
+                //Console.WriteLine(report);
+                int lengthOfAddedPosts = 12;
+                int indexOfAddedPosts = report.IndexOf("added posts:");
+                string beforeposts = report.Substring(0, indexOfAddedPosts);
+                string remainingReport = report.Substring(indexOfAddedPosts + lengthOfAddedPosts);
+                int indexOfPostTitle = remainingReport.IndexOf("post title:");
+                int indexOfPostContent = remainingReport.IndexOf("post content:");
+                ListBox singleModeratorPosts = new ListBox();
+                while (indexOfPostTitle >= 0)
+                {
+                    Expander exp = new Expander();
+                    exp.Header = remainingReport.Substring(indexOfPostTitle, indexOfPostContent);
+                    remainingReport = remainingReport.Substring(indexOfPostContent);
+                    indexOfPostTitle = remainingReport.IndexOf("post title:");
+                    indexOfPostContent = remainingReport.IndexOf("post content:");
+                    if (indexOfPostTitle == -1)
+                        indexOfPostTitle = remainingReport.Length; 
+                    exp.Content = remainingReport.Substring(indexOfPostContent, indexOfPostTitle);
+                    remainingReport = remainingReport.Substring(indexOfPostTitle);
+                    indexOfPostTitle = remainingReport.IndexOf("post title:");
+                    indexOfPostContent = remainingReport.IndexOf("post content:");
+                    singleModeratorPosts.Items.Add(exp);
+                }
+                reportListBox.Items.Add(beforeposts);
+                reportListBox.Items.Add(singleModeratorPosts);
+                height = height + singleModeratorPosts.Height;
             }
+            reportListBox.Height = height;
         }
 
         private void logout(String nameLogout)
